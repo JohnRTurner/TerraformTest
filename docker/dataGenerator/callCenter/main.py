985fb7f93d62baf76584af8e60a88b1c71d03f0b
John Turner <john.turner@aiven.io>
2023-11-27 12:46:45 -0500

    Adding AWS Docker

--- docker/dataGenerator/callCenter/main.py
+++ docker/dataGenerator/callCenter/main.py
@@ -1,8 +1,8 @@
 import argparse
 import random
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import as_completed
 from datetime import datetime, timedelta
-from threading import Semaphore
+from boundedexecutor import BoundedExecutor
 
 import numpy as np
 import psycopg2.pool
@@ -17,7 +17,6 @@ fake = Faker()
 epic_date = datetime(2022, 1, 1).date()
 today = datetime.today().replace(tzinfo=pytz.utc)
 days_to_cycle = 22
-
 
 
 # Function to generate mock data for agents
@@ -79,7 +78,7 @@ def max_date(pool, table_name):
         with conn:
             with conn.cursor() as cur:
                 cur.execute(
-                    f"select date_trunc('day',coalesce(max(call_date + interval '1 day'),current_timestamp(3) - interval '365 days')) from {table_name}")
+                    f"select date_trunc('day',coalesce(max(call_date) + interval '1 day',current_timestamp(3) - interval '365 days')) from {table_name}")
                 l_start_dt = cur.fetchone()[0]
                 if l_start_dt.weekday() > 4:
                     l_start_dt = l_start_dt - timedelta(days=(7 - l_start_dt.weekday()))
@@ -143,7 +142,7 @@ args = parser.parse_args()
 
 # Create a connection pool
 connection_pool = psycopg2.pool.ThreadedConnectionPool(
-    minconn=int(args.num_jobs) + 1,
+    minconn=(int(args.num_jobs) * 2) + 1,
     maxconn=(int(args.num_jobs) * 2) + 1,
     dbname=args.dbname,
     user=args.user,
@@ -152,81 +151,61 @@ connection_pool = psycopg2.pool.ThreadedConnectionPool(
     port=int(args.port),
 )
 
-batch_size = 100
+batch_size = 1000
 n_workers = int(args.num_jobs)
-n_queue = n_workers * 2
-semaphore = Semaphore(n_queue)
-print(f"Starting with {n_workers} workers and {n_queue} queue length.")
-
-
-def task_complete_callback(fut):
-    global semaphore
-    semaphore.release()
-    if hasattr(fut, 'results'):
-        print(fut.result())
-
+n_queue = n_workers * 4
+executor = BoundedExecutor(n_workers, n_queue)
+print(f"Starting with {n_workers} workers and {n_queue} queue length.", flush=True)
 
 
 # Generate data for agents table
 agents_count = get_count(connection_pool, "agents")
-with ThreadPoolExecutor(n_workers) as executor:
-    futures = []
-    min_batch = batch_size if (batch_size < int(args.num_agents)) else int(args.num_agents)
-    while agents_count < int(args.num_agents):
-        global semaphore
-        semaphore.acquire()
-        future = executor.submit(gen_agent, connection_pool, min_batch)
-        future.add_done_callback(task_complete_callback)
-        futures.append(future)
-        agents_count += min_batch
-    for future in as_completed(futures):
-        pass
-    #     if hasattr(future, 'results'):
-    #         print(future.result())
+futures = []
+min_batch = batch_size if (batch_size < int(args.num_agents)) else int(args.num_agents)
+while agents_count < int(args.num_agents):
+    future = executor.submit(gen_agent, connection_pool, min_batch)
+    futures.append(future)
+    agents_count += min_batch
+for future in as_completed(futures):
+    pass
+    if hasattr(future, 'results'):
+        print(future.result())
 agents_count = get_count(connection_pool, "agents")
-print(f"Total Agents in Table: {agents_count}")
+print(f"Total Agents in Table: {agents_count}", flush=True)
 
 # Generate data for customers table
 customer_count = get_count(connection_pool, "customers")
-with ThreadPoolExecutor(n_workers) as executor:
-    futures = []
-    min_batch = batch_size if (batch_size < int(args.num_jobs)) else int(args.num_jobs)
-    while customer_count < int(args.num_customers):
-        global semaphore
-        semaphore.acquire()
-        future = executor.submit(gen_customer, connection_pool, min_batch)
-        future.add_done_callback(task_complete_callback)
-        futures.append(future)
-        customer_count += min_batch
-    for future in as_completed(futures):
-        pass
-    #     if hasattr(future, 'results'):
-    #         print(future.result())
+futures = []
+min_batch = batch_size if (batch_size < int(args.num_jobs)) else int(args.num_jobs)
+while customer_count < int(args.num_customers):
+    future = executor.submit(gen_customer, connection_pool, min_batch)
+    futures.append(future)
+    customer_count += min_batch
+for future in as_completed(futures):
+    pass
+    if hasattr(future, 'results'):
+        print(future.result())
 customer_count = get_count(connection_pool, "customers")
-print(f"Total Customers in Table: {customer_count}")
+print(f"Total Customers in Table: {customer_count}", flush=True)
 
 start_dt = max_date(connection_pool, "calls")
 bus_days = np.busday_count(epic_date, start_dt.date())
-with ThreadPoolExecutor(n_workers) as executor:
-    futures = []
-    while start_dt < today:
-        global semaphore
-        semaphore.acquire()
-        future = executor.submit(gen_call_day,
-                                 connection_pool,
-                                 start_dt,
-                                 bus_days % days_to_cycle,
-                                 int(args.num_agents),
-                                 int(args.num_customers))
-        future.add_done_callback(task_complete_callback)
-        futures.append(future)
-        start_dt += timedelta(days=1)
-        if start_dt.weekday() > 4:
-            start_dt += timedelta(days=(7 - start_dt.weekday()))
-        bus_days += 1
-    for future in as_completed(futures):
-        pass
-    #     if hasattr(future, 'results'):
-    #         print(future.result())
+futures = []
+while start_dt < today:
+    future = executor.submit(gen_call_day,
+                             connection_pool,
+                             start_dt,
+                             bus_days % days_to_cycle,
+                             int(args.num_agents),
+                             int(args.num_customers))
+    futures.append(future)
+    start_dt += timedelta(days=1)
+    if start_dt.weekday() > 4:
+        start_dt += timedelta(days=(7 - start_dt.weekday()))
+    bus_days += 1
+for future in as_completed(futures):
+    pass
+    if hasattr(future, 'results'):
+        print(future.result())
 
 # Can add more to continue running today then staying real-time...
