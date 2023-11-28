7fcbf67ae6758a6707140fc144abef413c0e9412
John Turner <john.turner@aiven.io>
2023-11-28 09:14:16 -0500

    Adding AWS Docker

--- docker/dataGenerator/callCenter/main.py
+++ docker/dataGenerator/callCenter/main.py
@@ -1,24 +1,36 @@
 import argparse
 import random
-from concurrent.futures import as_completed
 from datetime import datetime, timedelta
+from multiprocessing import Process, Queue, freeze_support
 from time import sleep
 
-from boundedexecutor import BoundedExecutor
-
 import numpy as np
-import psycopg2.pool
 import pytz
 from faker import Faker
+
+import psycopg2.pool
 from psycopg2.extensions import register_adapter, AsIs
+from psycopg2.extras import execute_batch
 
 register_adapter(np.int64, AsIs)
 
 # Initialize Faker to generate fake data
 fake = Faker()
-epic_date = datetime(2022, 1, 1).date()
-today = datetime.today().replace(tzinfo=pytz.utc)
-days_to_cycle = 22
+
+
+def worker(inp, out, dbname, user, password, host, port):
+    # Create a connection pool
+    l_pool = psycopg2.pool.ThreadedConnectionPool(
+        minconn=5,
+        maxconn=5,
+        dbname=dbname,
+        user=user,
+        password=password,
+        host=host,
+        port=port,
+    )
+    for func1, args1 in iter(inp.get, 'STOP'):
+        out.put(func1(l_pool, *args1))
 
 
 # Function to generate mock data for agents
@@ -51,14 +63,14 @@ def generate_customers_data(record_count):
 def insert_data(pool, table_name, col_list, data):
     con1 = None
     try:
-        con1 = pool.getconn
+        con1 = pool.getconn()
     except Exception as e:
         print(f"WAITING FOR CONNECTION FOR {table_name} {e}", flush=True)
 
     while con1 is None:
         sleep(1)
         try:
-            con1 = pool.getconn
+            con1 = pool.getconn()
         except Exception as e:
             print(f"WAITING FOR CONNECTION FOR {table_name} {e}", flush=True)
     try:
@@ -66,8 +78,8 @@ def insert_data(pool, table_name, col_list, data):
             with con1.cursor() as cur:
                 placeholders = ', '.join(['%s'] * len(data[0]))
                 insert_query = f"INSERT INTO {table_name}({col_list}) VALUES ({placeholders})"
-                x = cur.executemany(insert_query, data)
-                print(f"inserted into {table_name} {x} rows.", flush=True)
+                execute_batch(cur, insert_query, data)
+                print(f"inserted into {table_name}.", flush=True)
     except Exception as e:
         print(f"INSERT INTO {table_name} FAILED {e}", flush=True)
     finally:
@@ -94,7 +106,7 @@ def max_date(pool, table_name):
         with conn:
             with conn.cursor() as cur:
                 cur.execute(
-                    f"select date_trunc('day',coalesce(max(call_date) + interval '1 day',current_timestamp(3) - interval '365 days')) from {table_name}")
+                    f"select date_trunc('day',coalesce(max(call_date) + interval '1 day',current_timestamp(3) - interval '90 days')) from {table_name}")
                 l_start_dt = cur.fetchone()[0]
                 if l_start_dt.weekday() > 4:
                     l_start_dt = l_start_dt - timedelta(days=(7 - l_start_dt.weekday()))
@@ -104,19 +116,15 @@ def max_date(pool, table_name):
 
 
 def gen_agent(pool, batch_sz):
-    print("Started Generating Agent Data", flush=True)
     dat = generate_agents_data(batch_sz)
-    print("Finished Generating Agent Data, Started Inserting", flush=True)
     insert_data(pool, 'agents', "agent_name, department, hire_date, is_active", dat)
-    print("Finished Inserting Agent Data", flush=True)
+    return f"gen_agent completed {batch_sz}"
 
 
 def gen_customer(pool, batch_sz):
-    print("Started Generating Customer Data", flush=True)
     dat = generate_customers_data(batch_sz)
-    print("Finished Generating Customer Data, Started Inserting", flush=True)
     insert_data(pool, 'customers', "customer_name, phone_number, email, address, city, country", dat)
-    print("Finished Inserting Customer Data", flush=True)
+    return f"gen_customer completed {batch_sz}"
 
 
 def gen_call_day(pool, start_date, bus_day, num_agents, num_customers, b_size):
@@ -125,11 +133,10 @@ def gen_call_day(pool, start_date, bus_day, num_agents, num_customers, b_size):
     start_date += timedelta(hours=8)  # start at 8am
     for agent in range(1, num_agents + 1):
         s_dt = start_date
-        # wrong
         customer = (bus_day * num_agents) + agent
         call_list = []
         while customer < num_customers:
-            call_length = timedelta(minutes=random.randint(5, 9))
+            call_length = timedelta(minutes=random.randint(4, 12))  # Call Duration
             call_list.append(
                 (agent,
                  customer,
@@ -146,7 +153,7 @@ def gen_call_day(pool, start_date, bus_day, num_agents, num_customers, b_size):
                             call_list)
                 call_list = []
             s_dt += call_length
-            s_dt += timedelta(minutes=random.randint(1, 2))
+            s_dt += timedelta(minutes=random.randint(1, 5))  # Pause Between Calls
             customer += num_agents * days_to_cycle
         if len(call_list) > 0:
             insert_data(pool, "calls",
@@ -154,84 +161,88 @@ def gen_call_day(pool, start_date, bus_day, num_agents, num_customers, b_size):
                         call_list)
 
 
-# Parse command-line arguments
-parser = argparse.ArgumentParser(description='Insert mock data into PostgresSQL tables')
-parser.add_argument('--dbname', help='Database name', required=True)
-parser.add_argument('--user', help='Database user', required=True)
-parser.add_argument('--password', help='Database password', required=True)
-parser.add_argument('--host', help='Database host', required=True)
-parser.add_argument('--port', help='Database port', required=True)
-parser.add_argument('--num_jobs', help='Number of jobs to run parallel', required=True)
-parser.add_argument('--num_agents', help='Number of agents', required=True)
-parser.add_argument('--num_customers', help='Number of customers', required=True)
-args = parser.parse_args()
+if __name__ == '__main__':
+    freeze_support()
 
-# Create a connection pool
-connection_pool = psycopg2.pool.ThreadedConnectionPool(
-    minconn=(int(args.num_jobs) * 2) + 1,
-    maxconn=(int(args.num_jobs) * 2) + 1,
-    dbname=args.dbname,
-    user=args.user,
-    password=args.password,
-    host=args.host,
-    port=int(args.port),
-)
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description='Insert mock data into PostgresSQL tables')
+    parser.add_argument('--dbname', help='Database name', required=True)
+    parser.add_argument('--user', help='Database user', required=True)
+    parser.add_argument('--password', help='Database password', required=True)
+    parser.add_argument('--host', help='Database host', required=True)
+    parser.add_argument('--port', help='Database port', required=True)
+    parser.add_argument('--num_jobs', help='Number of jobs to run parallel', required=True)
+    parser.add_argument('--num_agents', help='Number of agents', required=True)
+    parser.add_argument('--num_customers', help='Number of customers', required=True)
+    args = parser.parse_args()
 
-batch_size = 1000
-n_workers = int(args.num_jobs)
-n_queue = n_workers * 4
-executor = BoundedExecutor(n_workers, n_queue)
-print(f"Starting with {n_workers} workers and {n_queue} queue length.", flush=True)
+    # Create a connection pool
+    connection_pool = psycopg2.pool.ThreadedConnectionPool(
+        minconn=(int(args.num_jobs) * 6) + 1,
+        maxconn=(int(args.num_jobs) * 6) + 1,
+        dbname=args.dbname,
+        user=args.user,
+        password=args.password,
+        host=args.host,
+        port=int(args.port),
+    )
 
-# Generate data for agents table
-agents_count = get_count(connection_pool, "agents")
-futures = []
-min_batch = batch_size if (batch_size < int(args.num_agents)) else int(args.num_agents)
-while agents_count < int(args.num_agents):
-    future = executor.submit(gen_agent, connection_pool, min_batch)
-    futures.append(future)
-    agents_count += min_batch
-for future in as_completed(futures):
-    pass
-    if hasattr(future, 'results'):
-        print(future.result())
-agents_count = get_count(connection_pool, "agents")
-print(f"Total Agents in Table: {agents_count}", flush=True)
+    epic_date = datetime(2022, 1, 1).date()
+    today = datetime.today().replace(tzinfo=pytz.utc)
+    days_to_cycle = 22
+    batch_size = 1000
+    n_workers = int(args.num_jobs)
 
-# Generate data for customers table
-customer_count = get_count(connection_pool, "customers")
-futures = []
-min_batch = batch_size if (batch_size < int(args.num_customers)) else int(args.num_customers)
-while customer_count < int(args.num_customers):
-    future = executor.submit(gen_customer, connection_pool, min_batch)
-    futures.append(future)
-    customer_count += min_batch
-for future in as_completed(futures):
-    pass
-    if hasattr(future, 'results'):
-        print(future.result())
-customer_count = get_count(connection_pool, "customers")
-print(f"Total Customers in Table: {customer_count}", flush=True)
+    # pool = mp.Pool(processes=n_workers)
+    print(f"Starting with {n_workers} workers.", flush=True)
+    task_queue = Queue()
+    done_queue = Queue()
+    for i in range(n_workers):
+        Process(target=worker, args=(task_queue, done_queue, args.dbname, args.user, args.password, args.host, int(args.port))).start()
 
-start_dt = max_date(connection_pool, "calls")
-bus_days = np.busday_count(epic_date, start_dt.date())
-futures = []
-while start_dt < today:
-    future = executor.submit(gen_call_day,
-                             connection_pool,
-                             start_dt,
-                             bus_days % days_to_cycle,
-                             int(args.num_agents),
-                             int(args.num_customers),
-                             batch_size)
-    futures.append(future)
-    start_dt += timedelta(days=1)
-    if start_dt.weekday() > 4:
-        start_dt += timedelta(days=(7 - start_dt.weekday()))
-    bus_days += 1
-for future in as_completed(futures):
-    pass
-    if hasattr(future, 'results'):
-        print(future.result())
+    # Generate data for agents table
+    agents_count = get_count(connection_pool, "agents")
+    min_batch = batch_size if (batch_size < int(args.num_agents)) else int(args.num_agents)
+    tskCnt = 0
+    while agents_count < int(args.num_agents):
+        task_queue.put((gen_agent, [min_batch]))
+        agents_count += min_batch
+        tskCnt += 1
+    for i in range(tskCnt):
+        print(f"{done_queue.get()}")
+    agents_count = get_count(connection_pool, "agents")
+    print(f"Total Agents in Table: {agents_count}", flush=True)
 
-# Can add more to continue running today then staying real-time...
+    # Generate data for customers table
+    customer_count = get_count(connection_pool, "customers")
+    min_batch = batch_size if (batch_size < int(args.num_customers)) else int(args.num_customers)
+    tskCnt = 0
+    while customer_count < int(args.num_customers):
+        task_queue.put((gen_customer, [min_batch]))
+        customer_count += min_batch
+        tskCnt += 1
+    for i in range(tskCnt):
+        print(f"{done_queue.get()}")
+    customer_count = get_count(connection_pool, "customers")
+    print(f"Total Customers in Table: {customer_count}", flush=True)
+
+    # Generate data for calls table
+    start_dt = max_date(connection_pool, "calls")
+    bus_days = np.busday_count(epic_date, start_dt.date())
+    tskCnt = 0
+    while start_dt < today:
+        task_queue.put((gen_call_day,
+                       (start_dt, bus_days % days_to_cycle,
+                        int(args.num_agents), int(args.num_customers), batch_size)))
+        start_dt += timedelta(days=1)
+        if start_dt.weekday() > 4:
+            start_dt += timedelta(days=(7 - start_dt.weekday()))
+        bus_days += 1
+        tskCnt += 1
+    for i in range(tskCnt):
+        print(f"{done_queue.get()}")
+
+    # Stop mulitithreaded prpocs
+    for i in range(n_workers):
+        task_queue.put('STOP')
+    # Can add more to continue running today then staying real-time...
